f08c324b68a0149f6541962c7b028979dfa7c36c
Ayobami Alaran A. <ayobamidele006@gmail.com>
2023-09-30 15:36:42 +0000

    Add users creation endpoint

---
+++ controllers/UsersController.js
@@ -0,0 +1,29 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    try {
+      const { email, password } = req.body;
+      if (!email) return res.status(400).json({ error: 'Missing email' });
+      if (!password) return res.status(400).json({ error: 'Missing password' });
+
+      const users = await dbClient.db.collection('users');
+      const user = await users.findOne({ email });
+      if (user) return res.status(400).json({ error: 'Already exists' });
+
+      const hashedPassword = sha1(password);
+      const result = await users.insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      return res.status(201).json({ id: result.insertedId, email });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
+
+module.exports = UsersController;
